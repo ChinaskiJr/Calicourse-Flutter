f2a40790820727a2a123fc3ecabfaab08c4d96e4
ChinaskiJr <robin.colombier@protonmail.com>
2020-01-25 17:45:42 +0100

    Reformat article and shop entities to be API friendly

--- lib/models/Article.dart
+++ lib/models/Article.dart
@@ -1,6 +1,7 @@
 import 'package:calicourse_front/models/Shop.dart';
 
 class Article {
+  int       id;
   String    title;
   bool      bought;
   bool      archived;
@@ -13,8 +14,20 @@ class Article {
   Article.empty();
 
   /// All fields constructor for [Article]
-  Article.build(this.title, this.bought, this.archived, this.createdAt, this.boughtAt,
-      this.comment, this.shop);
+  Article.build({this.id, this.title, this.bought, this.archived, this.createdAt,
+    this.boughtAt, this.comment, this.shop});
+
+  factory Article.fromJson(Map <String, dynamic> json) {
+    return Article.build(
+      id:         json['id'],
+      title:      json['title'],
+      bought:     json['bought'],
+      archived:   json['archived'],
+      createdAt:  json['createdAt'],
+      boughtAt:   json['boughtAt'],
+      comment:    json['comment'],
+    );
+  }
 
   @override
   String toString() {
--- lib/models/Shop.dart
+++ lib/models/Shop.dart
@@ -1,17 +1,25 @@
 import 'package:calicourse_front/models/Article.dart';
 
 class Shop {
+  int             id;
   String          name;
-  List<Article>   articles;
+  List<Article>  articles;
 
   /// Empty constructor for [Shop]
   Shop.empty();
 
   /// Full fields constructor for [Shop]
-  Shop.build(this.name, this.articles);
+  Shop.build({this.id, this.name});
+
+  factory Shop.fromJson(Map<String, dynamic> json) {
+    return Shop.build(
+        id:   json['id'],
+        name: json['name'],
+    );
+  }
 
   @override
   String toString() {
-    return 'Shop{name: $name, articles: $articles}';
+    return 'Shop{id: $id, name: $name, articles: $articles}';
   }
 }
