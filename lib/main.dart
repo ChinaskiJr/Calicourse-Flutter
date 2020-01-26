59548f168af3ee6143eb0e1526f6687ed3869527
ChinaskiJr <robin.colombier@protonmail.com>
2020-01-26 13:33:59 +0100

    Set the main colors in the parameters file

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calicourse_front/parameters/parameters.dart';
 import 'package:calicourse_front/widgets/HomePage.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Calicourse',
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        primarySwatch: mainColor,
       ),
       home: HomePage(title: 'Calicourse'),
     );
--- lib/parameters/parameters.dart
+++ lib/parameters/parameters.dart
@@ -1,1 +1,5 @@
-const double globalFontSize = 16.0;
+import 'package:flutter/material.dart';
+
+const double globalFontSize     = 16.0;
+const Color mainColor           = Colors.lightGreen;
+const Color secondaryColor      = Colors.blueAccent;
