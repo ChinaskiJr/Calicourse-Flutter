c15567422b1e7d8a0e78c2ca10c60c184965a7bd
ChinaskiJr <robin.colombier@protonmail.com>
2020-02-06 21:55:26 +0100

    Display comments on pressed

--- lib/widgets/pages/ShopPage.dart
+++ lib/widgets/pages/ShopPage.dart
@@ -15,44 +15,84 @@ class ShopPage extends StatefulWidget {
 
 class _ShopPageState extends State<ShopPage> {
 
-  Shop shop;
+  Shop shop = Shop.empty();
+  List<bool> displayCommentPressed  = [];
+  List<IconData> trailingIcons      = [];
 
   @override
   Widget build(BuildContext context) {
     final String shopId = ModalRoute.of(context).settings.arguments;
 
-    return FutureBuilder(
-      future: _loadShop(shopId),
-      builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(shop.name),
-            ),
-            body: ListView.builder(
-              shrinkWrap: true,
-              itemCount: shop.articles.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Text(shop.articles[index].title);
-              }
+    _loadShop(shopId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: (shop.id == null) ? CircularProgressIndicator() : Text(shop.name),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.05),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: (shop.articles == null) ? 0 : shop.articles.length,
+                itemBuilder: (BuildContext context, int index) {
+                  displayCommentPressed.add(false);
+                  trailingIcons.add(Icons.keyboard_arrow_down);
+                  return Dismissible(
+                    key: ValueKey('article_' + shop.articles[index].id.toString()),
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(5.0),
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                              title: Text(
+                                shop.articles[index].title
+                              ),
+                              trailing: (shop.articles[index].comment.isNotEmpty)
+                                ? IconButton(
+                                icon: Icon(trailingIcons[index]),
+                                onPressed: () {
+                                  setState(() {
+                                    if (displayCommentPressed[index] == true) {
+                                      displayCommentPressed[index] = false;
+                                      trailingIcons[index] = Icons.keyboard_arrow_down;
+                                    } else {
+                                      displayCommentPressed[index] = true;
+                                      trailingIcons[index] = Icons.keyboard_arrow_up;
+                                    }
+                                  });
+                                })
+                                : null,
+                            ),
+                            (displayCommentPressed[index])
+                              ? Padding(
+                                  padding: EdgeInsets.only(bottom: 10.0),
+                                  child: Text(shop.articles[index].comment),
+                                )
+                              : Container()
+                          ],
+                        ),
+                      )
+                    )
+                  );
+                })
             )
-          );
-        } else {
-          return SizedBox(
-            height: 40.0,
-            width: 40.0,
-            child: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-      },
+          ],
+        ),
+      )
     );
   }
-
+  /// Load the shop from the API and then set it in the State.
+  /// Catch [HttpException] and display Fatal Error if needed.
   Future<void> _loadShop(String shopId) async {
     try {
-      this.shop = await HttpHelper.getShop(shopId);
+      Shop shop = await HttpHelper.getShop(shopId);
+      setState(() {
+        this.shop = shop;
+      });
     } on HttpException catch(exception, stackTrace) {
       print(stackTrace);
       FatalAlertDialog.showFatalError(exception.message, context);
