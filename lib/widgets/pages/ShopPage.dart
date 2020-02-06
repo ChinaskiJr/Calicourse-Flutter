0dd93f7e632fecaa262fcf4bb3f2b0bb5398775c
ChinaskiJr <robin.colombier@protonmail.com>
2020-02-06 23:00:11 +0100

    Creates the second Listview for articles that been bought

--- lib/widgets/pages/ShopPage.dart
+++ lib/widgets/pages/ShopPage.dart
@@ -27,7 +27,7 @@ class _ShopPageState extends State<ShopPage> {
     if (shop.id == null) {
       _loadShop(shopId);
     }
-
+    // TODO : Refactoriser les deux ListView (au moins les Cards)
     return Scaffold(
       appBar: AppBar(
         title: (shop.id == null) ? CircularProgressIndicator() : Text(shop.name),
@@ -37,69 +37,189 @@ class _ShopPageState extends State<ShopPage> {
           children: <Widget>[
             Container(
               padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.05),
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: (shop.articles == null) ? 0 : shop.articles.length,
-                itemBuilder: (BuildContext context, int index) {
-                  displayCommentPressed.add(false);
-                  trailingIcons.add(Icons.keyboard_arrow_down);
-                  // ARTICLES TO BUY
-                  if (!shop.articles[index].bought) {
-                    return Dismissible(
-                      key: ValueKey('article_' + shop.articles[index].id.toString()),
-                      background: Card(
-                        color: Colors.green,
-                        child: Icon(
-                          Icons.check,
-                          color: Colors.white,
-                        )
-                      ),
-                      onDismissed: (DismissDirection direction) async {
-                        setState(() {
-                          shop.articles[index].bought = true;
-                        });
-                        await _processPutArticle(shop.articles[index]);
-                      },
-                      child: Card(
-                        child: Padding(
-                          padding: EdgeInsets.all(5.0),
-                          child: Column(
-                            children: <Widget>[
-                              ListTile(
-                                title: Text(
-                                  shop.articles[index].title
-                                ),
-                                trailing: (shop.articles[index].comment.isNotEmpty)
-                                  ? IconButton(
-                                  icon: Icon(trailingIcons[index]),
-                                  onPressed: () {
-                                    setState(() {
-                                      if (displayCommentPressed[index] == true) {
-                                        displayCommentPressed[index] = false;
-                                        trailingIcons[index] = Icons.keyboard_arrow_down;
-                                      } else {
-                                        displayCommentPressed[index] = true;
-                                        trailingIcons[index] = Icons.keyboard_arrow_up;
-                                      }
-                                    });
-                                  })
-                                  : null,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "À acheter",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: (shop.articles == null) ? 0 : shop.articles.length,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (BuildContext context, int index) {
+                      displayCommentPressed.add(false);
+                      trailingIcons.add(Icons.keyboard_arrow_down);
+                      // ARTICLES TO BUY
+                      if (!shop.articles[index].bought) {
+                        return Dismissible(
+                          key: ValueKey('article_' + shop.articles[index].id.toString()),
+                          background: Card(
+                            color: Colors.green,
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 35.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.check,
+                                    color: Colors.white,
+                                  ),
+                                  Icon(
+                                    Icons.check,
+                                    color: Colors.white,
+                                  )
+                                ],
                               ),
-                              (displayCommentPressed[index])
-                                ? Padding(
-                                padding: EdgeInsets.only(bottom: 10.0),
-                                child: Text(shop.articles[index].comment),
-                              )
-                                : Container()
-                            ],
+                            )
                           ),
-                        )
-                      )
-                    );
-                  } else {
-                    return Container();
-                  }
-                })
+                          onDismissed: (DismissDirection direction) async {
+                            setState(() {
+                              shop.articles[index].bought = true;
+                            });
+                            await _processPutArticle(shop.articles[index]);
+                          },
+                          child: Card(
+                            child: Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text(
+                                      shop.articles[index].title
+                                    ),
+                                    trailing: (shop.articles[index].comment.isNotEmpty)
+                                      ? IconButton(
+                                      icon: Icon(trailingIcons[index]),
+                                      onPressed: () {
+                                        setState(() {
+                                          if (displayCommentPressed[index] == true) {
+                                            displayCommentPressed[index] = false;
+                                            trailingIcons[index] = Icons.keyboard_arrow_down;
+                                          } else {
+                                            displayCommentPressed[index] = true;
+                                            trailingIcons[index] = Icons.keyboard_arrow_up;
+                                          }
+                                        });
+                                      })
+                                      : null,
+                                  ),
+                                  (displayCommentPressed[index])
+                                    ? Padding(
+                                    padding: EdgeInsets.only(bottom: 10.0),
+                                    child: Text(shop.articles[index].comment),
+                                  )
+                                    : Container()
+                                ],
+                              ),
+                            )
+                          )
+                        );
+                      } else {
+                        return Container();
+                      }
+                    })
+                ],
+              )
+            ),
+            Container(
+              padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.05),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Acheté",
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: (shop.articles == null) ? 0 : shop.articles.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      displayCommentPressed.add(false);
+                      trailingIcons.add(Icons.keyboard_arrow_down);
+                      // ARTICLES TO BUY
+                      if (shop.articles[index].bought) {
+                        return Dismissible(
+                          key: ValueKey('article_' + shop.articles[index].id.toString()),
+                          background: Card(
+                            color: Colors.blue,
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 35.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.add_shopping_cart,
+                                    color: Colors.white,
+                                  ),
+                                  Icon(
+                                    Icons.add_shopping_cart,
+                                    color: Colors.white,
+                                  )
+                                ],
+                              ),
+                            )
+                          ),
+                          onDismissed: (DismissDirection direction) async {
+                            setState(() {
+                              shop.articles[index].bought = false;
+                            });
+                            await _processPutArticle(shop.articles[index]);
+                          },
+                          child: Card(
+                            color: Colors.blueGrey,
+                            child: Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text(
+                                      shop.articles[index].title,
+                                      style: TextStyle(
+                                        color: Colors.white
+                                      ),
+                                    ),
+                                    trailing: (shop.articles[index].comment.isNotEmpty)
+                                      ? IconButton(
+                                      icon: Icon(trailingIcons[index]),
+                                      onPressed: () {
+                                        setState(() {
+                                          if (displayCommentPressed[index] == true) {
+                                            displayCommentPressed[index] = false;
+                                            trailingIcons[index] = Icons.keyboard_arrow_down;
+                                          } else {
+                                            displayCommentPressed[index] = true;
+                                            trailingIcons[index] = Icons.keyboard_arrow_up;
+                                          }
+                                        });
+                                      })
+                                      : null,
+                                  ),
+                                  (displayCommentPressed[index])
+                                    ? Padding(
+                                    padding: EdgeInsets.only(bottom: 10.0),
+                                    child: Text(shop.articles[index].comment),
+                                  )
+                                    : Container()
+                                ],
+                              ),
+                            )
+                          )
+                        );
+                      } else {
+                        return Container();
+                      }
+                    })
+                ],
+              )
             ),
           ],
         ),
