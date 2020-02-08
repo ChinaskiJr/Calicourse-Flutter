be3ca060f82812f055bb2245bb231bca7352bee4
ChinaskiJr <robin.colombier@protonmail.com>
2020-02-08 15:18:29 +0100

    Fix scrollable issue on small devices

--- lib/widgets/pages/ArticlePage.dart
+++ lib/widgets/pages/ArticlePage.dart
@@ -44,100 +44,113 @@ class ArticlePageState extends State<ArticlePage>{
         ? Text("Modifier un article")
         : Text("Ajouter un article"),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.of(context).size.width * 0.05
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              TextFormField(
-                controller: _titleFieldConstructor,
-                decoration: const InputDecoration(
-                  icon: const Icon(Icons.create),
-                  hintText: "ex: Veggies",
-                  labelText: "Nom de l'article"
-                ),
-                validator: (String value) {
-                  if (value.isEmpty) {
-                    return 'Ce champ est obligatoire';
-                  } else {
-                    return null;
-                  }
-                },
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportContraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: viewportContraints.maxHeight,
               ),
-              TextFormField(
-                controller: _commentFieldConstructor,
-                decoration: const InputDecoration(
-                  icon: const Icon(Icons.description),
-                  hintText: "ex: Fais attention, ils ont été changé de rayon ! <3",
-                  labelText: "Description",
-                  alignLabelWithHint: true
-                ),
-                maxLines: 6,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 15.0),
-                child: Row(
-                  mainAxisAlignment: (article == null)
-                  ? MainAxisAlignment.center
-                  : MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    (article != null)
-                      ? RaisedButton(
-                      color: Colors.red,
-                      splashColor: Colors.white,
-                      onPressed: () async {
-                        await _processDeleteArticle(article);
-                        // True cause we need to refresh the page
-                        Navigator.of(context).pop(true);
-                      },
-                      child: Text(
-                        'Supprimer',
-                        style: TextStyle(
-                          color: Colors.white
+              child: IntrinsicHeight(
+                child: Padding(
+                    padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.05
+                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        TextFormField(
+                          controller: _titleFieldConstructor,
+                          decoration: const InputDecoration(
+                            icon: const Icon(Icons.create),
+                            hintText: "ex: Veggies",
+                            labelText: "Nom de l'article"
+                          ),
+                          validator: (String value) {
+                            if (value.isEmpty) {
+                              return 'Ce champ est obligatoire';
+                            } else {
+                              return null;
+                            }
+                          },
                         ),
-                      ),
+                        TextFormField(
+                          controller: _commentFieldConstructor,
+                          decoration: const InputDecoration(
+                            icon: const Icon(Icons.description),
+                            hintText: "ex: Fais attention, ils ont été changé de rayon ! <3",
+                            labelText: "Description",
+                            alignLabelWithHint: true
+                          ),
+                          maxLines: 6,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 15.0),
+                          child: Row(
+                            mainAxisAlignment: (article == null)
+                              ? MainAxisAlignment.center
+                              : MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              (article != null)
+                                ? RaisedButton(
+                                color: Colors.red,
+                                splashColor: Colors.white,
+                                onPressed: () async {
+                                  await _processDeleteArticle(article);
+                                  // True cause we need to refresh the page
+                                  Navigator.of(context).pop(true);
+                                },
+                                child: Text(
+                                  'Supprimer',
+                                  style: TextStyle(
+                                    color: Colors.white
+                                  ),
+                                ),
+                              )
+                                : Container(),
+                              RaisedButton(
+                                child: Text(
+                                  (article == null) ? 'Ajouter' : 'Modifier',
+                                  style: TextStyle(
+                                    color: Colors.white
+                                  ),
+                                ),
+                                color: mainColor,
+                                splashColor: Colors.white,
+                                onPressed: () async {
+                                  if (_formKey.currentState.validate()) {
+                                    if (article == null) {
+                                      // New article
+                                      Article newArticle = Article.create(
+                                        _titleFieldConstructor.text,
+                                        _commentFieldConstructor.text
+                                      );
+                                      await _processAddArticle(newArticle);
+                                    } else {
+                                      // Update Article
+                                      article.title = _titleFieldConstructor.text;
+                                      article.comment = _commentFieldConstructor.text;
+                                      await _processPutArticle(article);
+                                    }
+                                    // False cause no need to refresh the page
+                                    Navigator.of(context).pop(false);
+                                  }
+                                },
+                              ),
+                            ],
+                          )
+                        )
+                      ],
                     )
-                      : Container(),
-                    RaisedButton(
-                      child: Text(
-                        (article == null) ? 'Ajouter' : 'Modifier',
-                        style: TextStyle(
-                          color: Colors.white
-                        ),
-                      ),
-                      color: mainColor,
-                      splashColor: Colors.white,
-                      onPressed: () async {
-                        if (_formKey.currentState.validate()) {
-                          if (article == null) {
-                            // New article
-                            Article newArticle = Article.create(
-                              _titleFieldConstructor.text,
-                              _commentFieldConstructor.text
-                            );
-                            await _processAddArticle(newArticle);
-                          } else {
-                            // Update Article
-                            article.title = _titleFieldConstructor.text;
-                            article.comment = _commentFieldConstructor.text;
-                            await _processPutArticle(article);
-                          }
-                          // False cause no need to refresh the page
-                          Navigator.of(context).pop(false);
-                        }
-                      },
-                    ),
-                  ],
-                )
-              )
-            ],
-          )
-        ),
-      ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      )
     );
   }
   /// Between the Http call and the Navigator.pop.
